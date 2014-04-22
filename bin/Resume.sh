e953d18f18aa5a7fd962d9bd0f5453568b804fbf
Alex WEb <plcgi1@gmail.com>
2014-04-22 21:58:57 +0300

    1

--- bin/Resume.sh
+++ bin/Resume.sh
@@ -1,1 +1,1 @@
-plackup -I/home/harper/www/woa-toolkit/apps/Resume/lib -p 3030 -a /home/harper/www/woa-toolkit/apps/Resume/psgi/Resume.psgi
+plackup -I/home/harper/www/woa-toolkit/apps/my-resume/lib -p 3030 -a /home/harper/www/woa-toolkit/apps/my-resume/psgi/Resume.psgi
